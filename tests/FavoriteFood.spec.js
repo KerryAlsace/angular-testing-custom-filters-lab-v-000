fb041f62ecdd05aabbbb011abedf6b5ad1ad881a
KerryAlsace <kerrydougherty@gmail.com>
2016-11-14 14:15:55 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,33 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+  }));
 
-	
+  it('should filter correctly by favorite food', function() {
+    var people = [
+      {
+        name: 'Kevin',
+        favoriteFood: 'Biscuits and Gravy'
+      },
+      {
+        name: 'Kerry',
+        favoriteFood: 'Apples'
+      },
+      {
+        name: 'Nikki',
+        favoriteFood: 'Trash'
+      }
+    ];
+
+    var filteredList = $filter('favoriteFood')(people, 'Trash');
+
+    expect(filteredList.length).toBe(1);
+    expect(filteredList[0].name).toBe('Nikki');
+  })
+
+  
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,19 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+  var $controller;
 
-	beforeEach(module('app'));
+  beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+  beforeEach(inject(function ($injector) {
+    $filter = $injector.get('$filter');
+  }));
+
+  it('should remove all vowels from a string', function() {
+    var string = 'minneanapolis';
+
+    var vowellessString = $filter('removeAllVowels')(string);
+
+    expect(vowellessString.length).toBe(7);
+    expect(vowellessString).toBe('mnnnpls');
+  });
 
 });
